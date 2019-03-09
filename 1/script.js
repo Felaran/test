2b0c84a676a97eaa6522b58bbed25bf5ec82f709
Eldar <eldaratabiev@yandex.ru>
2019-03-09 11:47:43 +0300

    Первый комит

---
+++ 1/script.js
@@ -0,0 +1,18 @@
+
+//Типы данных
+
+var number = 10;    //Число
+var string = "Name";   //Строка
+var sym = Symbol(); //Символ
+var boolean = true; //Логический тип данных
+null;
+undefined;
+var obj = {};
+
+let person = {
+        name: "Eldar",
+        age: 25,
+        isMarried: false
+};
+
+console.log(person.name);
